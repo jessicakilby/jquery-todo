dc7844663621010373520f6e597df0234aaa171a
jessica kilby <jessicakilby@gmail.com>
2016-10-29 12:21:45 -0500

    messages printing to dom, buttons for each mssg, delete button funcitoning

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,16 +2,32 @@
 
 let input = $("#input");
 let todo = $("#todo");
-var submit = $("#submit");
+let submit = $("#submit");
 
 $(document).ready(function(){
 
 	$( submit ).on( "click", function(event) {
 		event.preventDefault();
 		let output = `<div class="outputDiv">${input.val()}`;
-		output += `<button class="btn btn-success">Edit</button>`;
-		output += `<button class="btn btn-warning">Delete</button></div>`;
+		output += `<button type="button" class="btn btn-info completed">Completed</button>`;
+		output += `<button class="btn btn-success edit">Edit</button>`;
+		output += `<button class="btn btn-warning deleteBtn">Delete</button></div>`;
 		todo.append(output);
 	});
 
+	$(document).on("click", ".completed", function(){
+		console.log("hey complete");
+	});
+
+	$(document).on("click", ".edit", function(){
+		console.log("hey edit");
+	});
+
+	$(document).on("click", '.deleteBtn', function(event){
+		$(this).closest('div').remove();
+	});
+
+// }
+
 });
+
