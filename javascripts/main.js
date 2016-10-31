241628fb9e249af50179538121f10befcd4b132c
jessica kilby <jessicakilby@gmail.com>
2016-10-31 18:47:32 -0500

    initial fb setup

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,32 +2,45 @@
 
 let input = $("#input");
 let todo = $("#todo");
+let completedDiv = $("#completedDiv");
 let submit = $("#submit");
+let messages = [];
+let completed = [];
 
 $(document).ready(function(){
 
 	$( submit ).on( "click", function(event) {
 		event.preventDefault();
-		let output = `<div class="outputDiv">${input.val()}`;
-		output += `<button type="button" class="btn btn-info completed">Completed</button>`;
+		let output = `<div class="outputDiv"><span>${input.val()}</span>`;
+		output += `<input type="checkbox" class="completed">Completed</input>`;
 		output += `<button class="btn btn-success edit">Edit</button>`;
 		output += `<button class="btn btn-warning deleteBtn">Delete</button></div>`;
 		todo.append(output);
+		messages.push(output);
+		console.log("messages", messages);
+	});
+
+	$(document).on('click', '.edit', function(){
+	    $("#input").focus();
+	    $("#input").val("");
+	    let editItem = $("#input").val();
+	    console.log($(this).siblings('span').text(editItem));
 	});
 
 	$(document).on("click", ".completed", function(){
-		console.log("hey complete");
-	});
-
-	$(document).on("click", ".edit", function(){
-		console.log("hey edit");
+		//let spanText = $(this).closest('span');
+		//console.log("spanText", spanText.text());
+		//moved message div from todo to completed
+		let moveMessage = $(this).closest('div').remove();
+		//completed.push(moveMessage.text());
+		completedDiv.append(moveMessage);
+		console.log("completed", completed);
 	});
 
 	$(document).on("click", '.deleteBtn', function(event){
 		$(this).closest('div').remove();
 	});
 
-// }
 
 });
 
